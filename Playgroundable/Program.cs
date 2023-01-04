c1128a9ac2a2f5a68d8ca386783e2c8807550429
Daniel Paklin <dangrgen@gmail.com>
2023-01-04 19:45:41 +0600

    Пилим бота, пока не получается десериализовать

--- Playgroundable/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Playgroundable;
-
-Console.WriteLine(Brabras.Sum(5,6));
-Console.WriteLine(75);
-
-Console.WriteLine("rebase test");
---
+++ TgBot/Controllers/TgBotController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TgBot.Controllers;
+
+[ApiController]
+[Route("api/bot")]
+public class TelegramController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Update update)
+    {
+        var botClient = new TelegramBotClient("5818909579:AAHjXutJHotzfLBzLH3UqMN77t19y8zoATk");
+        if (update.Type == Telegram.Bot.Types.Enums.UpdateType.ChannelPost)
+        {
+            var messageId = update.ChannelPost.MessageId;
+            await botClient.DeleteMessageAsync(-1001521736518, messageId);
+        }
+        
+
+        return Ok();
+    }
+}
