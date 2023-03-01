40f5577d37036d7e9619224446f78d516448b12b
Daniel Paklin <Dangrgen@gmail.com>
2023-03-01 19:03:17 +0600

    не до конца (#3)

---
+++ LoggingPlayground/Program.cs
@@ -0,0 +1,18 @@
+using LoggingPlayground;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+var configurationBuilder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json");
+
+var configuration = configurationBuilder.Build();
+
+Log.Logger = new LoggerBuilder().WithApplicationName("FinMonitoringPlayground").Build(configuration);
+
+ConnectionStringsManager.ReadFromConfiguration(configuration);
+
+for (int i = 0; i < 150; i++)
+{
+    Log.Logger.Information("Тестовый лог #{1}", i);
+}
+
