1837ed4ff877f692d4e0986cec4ba3326e5bb51b
Daniel Paklin <dangrgen@gmail.com>
2023-04-07 21:09:18 +0600

    работает

--- FfmpegPlayground/Program.cs
+++ FfmpegPlayground/Program.cs
@@ -1,9 +1,9 @@
 ﻿using FFMpegCore;
 using FFMpegCore.Arguments;
 using FFMpegCore.Enums;
-using FFMpegCore.Pipes;
 
-var inputPath = "./TestVideos/TestVideo.webm";
+var inputPath = "./videos/TestVideoBefore.mp4";
+var outputPath = "./videos/TestVideoAfter.mp4";
 
 var text1 = @"
 Осмонкулов
@@ -25,72 +25,68 @@ var text4 = @"
 Смотрите прямо в камеру
 ";
 
-var targetSize = VideoSize.Original;
+try
+{
+    var targetSize = VideoSize.Original;
 
-var info = await FFProbe.AnalyseAsync(inputPath);
-var origHeight = info.PrimaryVideoStream.Height;
-var origWidth = info.PrimaryVideoStream.Width;
+    var info = await FFProbe.AnalyseAsync(inputPath);
+    var videoStream = info.PrimaryVideoStream;
+        
+    if (videoStream is not null)
+    {
+        Console.WriteLine($"Width: {videoStream.Width}");
+        Console.WriteLine($"Height: {videoStream.Height}");
+        Console.WriteLine($"CodecName: {videoStream.CodecName}");
+        Console.WriteLine($"PixelFormat: {videoStream.PixelFormat}");
 
-if (origHeight * origWidth > 921_600)
-    targetSize = VideoSize.Hd;
+        var origHeight = videoStream.Height;
+        var origWidth = videoStream.Width;
+        if (origHeight * origWidth > 921_600)
+            targetSize = VideoSize.Hd;
+    }
 
-var inputBytes = File.ReadAllBytes(inputPath);
-var inputStream = new MemoryStream(inputBytes);
-
-var inputPipe = new StreamPipeSource(inputStream);
-
-var streamSample = new MemoryStream();
-
-var outputPipe = new StreamPipeSink(streamSample);
-
-var ffMpegArgumentProcessor = FFMpegArguments
-    .FromPipeInput(inputPipe)
-    .OutputToPipe(outputPipe, options => options
-        .ForceFormat("webm")
-        .WithVideoFilters(filterOptions => filterOptions
-            .Scale(targetSize)
-            .DrawText(DrawTextOptions.Create($"{text1}", "Arial")
-                .WithParameter("box", "1")
-                .WithParameter("boxcolor", "black@0.5")
-                .WithParameter("fontcolor", "white")
-                .WithParameter("fontsize", "24")
-                .WithParameter("x", "0")
-                .WithParameter("y", "0")
-                .WithParameter("enable", "'between(t, 15, 20)'"))
-        )
-        .WithCustomArgument(@"-an")
-    );
-
-await ffMpegArgumentProcessor.ProcessAsynchronously();
-
-var outputPath = "./TestVideos/TestVideoHD.webm";
-
-await using var outputStream = File.Create(outputPath);
-await outputStream.WriteAsync(streamSample.GetBuffer());
-await outputStream.FlushAsync();
-
-//
-// .DrawText(DrawTextOptions.Create($"{text2}", "Arial")
-//         .WithParameter("enable", "'between(t, 5, 10)'")
-//         .WithParameter("box", "1")
-//         .WithParameter("boxcolor", "black@0.5")
-//         .WithParameter("fontcolor", "white")
-//         .WithParameter("fontsize", "24")
-//         .WithParameter("x", "(w-text_w)")
-//         .WithParameter("y", "0"))
-//     .DrawText(DrawTextOptions.Create($"{text3}", "Arial")
-//         .WithParameter("enable", "'between(t, 10, 15)'")
-//         .WithParameter("box", "1")
-//         .WithParameter("boxcolor", "black@0.5")
-//         .WithParameter("fontcolor", "white")
-//         .WithParameter("fontsize", "24")
-//         .WithParameter("x", "(w-text_w)")
-//         .WithParameter("y", "0"))
-//     .DrawText(DrawTextOptions.Create($"{text4}", "Arial")
-//         .WithParameter("enable", "'between(t, 15, 20)'")
-//         .WithParameter("box", "1")
-//         .WithParameter("boxcolor", "black@0.5")
-//         .WithParameter("fontcolor", "white")
-//         .WithParameter("fontsize", "24")
-//         .WithParameter("x", "(w-text_w)")
-//         .WithParameter("y", "0")
+    await FFMpegArguments
+        .FromFileInput(inputPath)
+        .OutputToFile(outputPath, true, options => options
+            .ForceFormat("mp4")
+            .WithVideoFilters(filterOptions => filterOptions
+                .Scale(targetSize)
+                .DrawText(DrawTextOptions.Create($"{text1}", "Arial")
+                    .WithParameter("box", "1")
+                    .WithParameter("boxcolor", "black@0.5")
+                    .WithParameter("fontcolor", "white")
+                    .WithParameter("fontsize", "24")
+                    .WithParameter("x", "0")
+                    .WithParameter("y", "0"))
+                .DrawText(DrawTextOptions.Create($"{text2}", "Arial")
+                    .WithParameter("enable", "'between(t, 3, 6)'")
+                    .WithParameter("box", "1")
+                    .WithParameter("boxcolor", "black@0.5")
+                    .WithParameter("fontcolor", "white")
+                    .WithParameter("fontsize", "24")
+                    .WithParameter("x", "(w-text_w)/2")
+                    .WithParameter("y", "0"))
+                .DrawText(DrawTextOptions.Create($"{text3}", "Arial")
+                    .WithParameter("enable", "'between(t, 6, 9)'")
+                    .WithParameter("box", "1")
+                    .WithParameter("boxcolor", "black@0.5")
+                    .WithParameter("fontcolor", "white")
+                    .WithParameter("fontsize", "24")
+                    .WithParameter("x", "(w-text_w)/2")
+                    .WithParameter("y", "0"))
+                .DrawText(DrawTextOptions.Create($"{text4}", "Arial")
+                    .WithParameter("enable", "'between(t, 9, 12)'")
+                    .WithParameter("box", "1")
+                    .WithParameter("boxcolor", "black@0.5")
+                    .WithParameter("fontcolor", "white")
+                    .WithParameter("fontsize", "24")
+                    .WithParameter("x", "(w-text_w)/2")
+                    .WithParameter("y", "0")
+                ))
+            .WithCustomArgument("-an")
+        ).ProcessAsynchronously();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
