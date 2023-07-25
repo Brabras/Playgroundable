db106e424f40abaf74244a1556e21f8aa3a5b929
Daniel Paklin <dangrgen@gmail.com>
2023-07-25 12:12:15 +0600

    Exept

--- ConsolePlayground/Program.cs
+++ ConsolePlayground/Program.cs
@@ -1,5 +1,9 @@
 ﻿using ConsolePlayground;
 
+var list = new List<long> { 1, 3, 5 };
+var anotherList = new List<long> { 1, 2, 6 };
+var except = list.Except(anotherList);
+Console.Write(string.Join(" ,", except));
 var example = new Example(1);
 
 var brabras = example.ToBrabrasExt();
