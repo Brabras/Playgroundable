c53a6e50b50590c5de66eb7516d4d3c0fd4e42d0
Daniel Paklin <dangrgen@gmail.com>
2024-09-16 23:37:57 +0600

    убрал большие файлы

--- ConsolePlayground/Program.cs
+++ ConsolePlayground/Program.cs
@@ -1,37 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿string? a = "brabras";
+string? b = null;
 
-var input = new LargeStruct<string>("Phone number: 123-456-7890");
-
-
-var returningNewReplaceResult = ReturningNewReplace(input);
-Console.WriteLine(returningNewReplaceResult.Value);
-Console.WriteLine();
-
-var returningInputReplaceResult = ReturningInputReplace(input);
-Console.WriteLine(returningInputReplaceResult.Value);
-Console.WriteLine();
-
-
-LargeStruct<string> ReturningNewReplace(LargeStruct<string> xml)
-{
-    var regex = new Regex(@"\d", RegexOptions.Multiline | RegexOptions.Compiled);
-
-    var result = new LargeStruct<string>(regex.Replace(xml.Value, "#"));
-
-    return result;
-}
-
-
-LargeStruct<string> ReturningInputReplace(LargeStruct<string> xml)
-{
-    var regex = new Regex(@"\d", RegexOptions.Multiline | RegexOptions.Compiled);
-
-    regex.Replace(xml.Value, "#");
-
-    return xml;
-}
-
-public struct LargeStruct<TValue>(TValue value)
-{
-    public TValue Value { get; } = value;
-}
+Console.WriteLine(b ?? a);
